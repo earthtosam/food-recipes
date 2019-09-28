dd2bb4a74d57c729776c288cb3c447da0267fcbb
Sam <samuel_ramirez_07@yahoo.com>
2019-09-27 17:30:13 -0700

    Changed package.json
    Changed package.json file to add homepage

---
+++ build/precache-manifest.cabe031f032edfc081914ce08da3b72a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00aee01a6a3a23f1253c98614134af6",
+    "url": "/food-recipes/index.html"
+  },
+  {
+    "revision": "8b0c1979c91c2521f1a0",
+    "url": "/food-recipes/static/css/main.3da55817.chunk.css"
+  },
+  {
+    "revision": "e269caec8435623e6b29",
+    "url": "/food-recipes/static/js/2.881939cd.chunk.js"
+  },
+  {
+    "revision": "8b0c1979c91c2521f1a0",
+    "url": "/food-recipes/static/js/main.92f50d9c.chunk.js"
+  },
+  {
+    "revision": "0e02eda8c5293b84e940",
+    "url": "/food-recipes/static/js/runtime~main.bf7c1f15.js"
+  }
+]);
